89e87c71ae07394b5a823ef75cc425d41153d695
falesmiletus <mixail.agafonov.1986@gmail.com>
2025-04-01 14:46:08 +0300

    add 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.Emit;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            againName:
+            Console.WriteLine("Write your name.");
+            Console.Write("Name - ");
+            string name = Console.ReadLine()!;
+            if(String.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Error. Please try again");
+                goto againName;
+            }
+            againAge:
+            Console.WriteLine("Write your age.");
+            Console.Write("Age - ");
+            if (!int.TryParse(Console.ReadLine(),out age))
+            {
+                Console.WriteLine("Error. Please try again");
+                goto againAge;
+            }
+
+            Console.WriteLine($"Your name is {name}. Your age is {age}");
+
+        }
+    }
+}
